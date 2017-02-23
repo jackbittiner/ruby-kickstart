1f4ce4098ed80e60c6359e72413d97ed71a74bfb
Jack Bittiner <jackbittiner@hotmail.com>
2017-02-23 10:29:26 +0000

    Session2 Challenge3&4 Complete

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,9 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+def get_squares(numbers)
+  array = []
+  numbers.each { |x| numbers.include?(x * x) ? array << x : nil }
+  return array.sort
+end
