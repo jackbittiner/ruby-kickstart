bf0752c6361eb38b51a27762dd937168cbb4020b
Jack Bittiner <jackbittiner@hotmail.com>
2017-02-23 23:08:54 +0000

    Session2 Challenge11 Complete

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,52 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+  attr_accessor :beers
+  def initialize(beers)
+    beers = 0  if beers < 0
+    beers = 99 if beers > 99
+    @beers = beers
+  end
+
+  def print_song
+    beers.downto 1 do |i|
+      song i
+    end
+  end
+
+  def song(n)
+    if n.zero?
+      String.new
+    else
+      puts "#{number_to_english n} #{bottle_or_s n} of beer on the wall,"        ,
+           "#{number_to_english n} #{bottle_or_s n} of beer,"                    ,
+           "Take one down, pass it around,"                         ,
+           "#{number_to_english n - 1} #{bottle_or_s n-1} of beer on the wall."
+    end
+  end
+
+  def number_to_english(n)
+  numbers_to_twenty = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  multiples_of_ten = ["zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    if  n >= 0 && n < 20
+      return numbers_to_twenty[n].capitalize
+    elsif n % 10 == 0
+      return multiples_of_ten[n/10].capitalize
+    else
+      return_this = "#{number_to_english n/10*10}-#{number_to_english n%10}".capitalize
+    end
+  end
+
+  def bottle_or_s(n)
+    n == 1 ? "bottle" : "bottles"
+  end
+
+end
