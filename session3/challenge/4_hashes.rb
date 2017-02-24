8491e1ee42d3e2e9c54e0f5b90fe9cac14c18d29
Jack Bittiner <jackbittiner@hotmail.com>
2017-02-24 16:58:33 +0000

    Session3 Challenge4 Complete

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,12 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(string)
+  hash = Hash.new
+  string.split.each_with_index do |x, index|
+    return if x == ""
+    hash[x] ||= index
+  end
+  return hash
+end
